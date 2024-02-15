94fafef49ed4e38cf6ca2a3eff9fe1ec80b856ef
Alish7175 <54021151+Alish7175@users.noreply.github.com>
2024-02-16 01:24:27 +0530

    feat: addded msw and axios and also written basic test

---
+++ src/ApiClient.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseURL = "https://dummyjson.com";
+
+export const axiosClient = axios.create({
+    baseURL,
+    headers: {
+        'X-Custom-Header': 'foobar',
+    },
+    withCredentials: false,
+})
+
